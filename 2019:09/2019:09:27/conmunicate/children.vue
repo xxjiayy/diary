d40f36b232c9e591e03de20b81e5b2f1183ea2a7
Xie jiayun <694056956@qq.com>
2019-09-29 17:35:38 +0800

    2019:09:29

---
+++ 2019:09/2019:09:27/conmunicate/children.vue
@@ -0,0 +1,19 @@
+<template>
+	<div id="app">
+		<h2>chidren component</h2>
+		<div>
+			<p><b>name:</b> {{name}}</p>
+		</div>
+	</div>
+</template>
+<script>
+import children form './children.html'
+export default {
+	el: '#app',
+	data: {
+	},
+	props: {
+		name: String
+	}
+}
+</script>
---
+++ 2019:09/2019:09:27/conmunicate/parent.vue
@@ -0,0 +1,19 @@
+<template>
+	<div id="app">
+		<h1>parent component</h1>
+		<children :name="msg"></children>
+	</div>
+</template>
+<script>
+	import children form './children'
+	var vm = new Vue({
+		el: '#app',
+		data: {
+			mag: 'joycee'
+		},
+		components: {
+			children
+		}
+	});
+	export default vm;
+</script>
